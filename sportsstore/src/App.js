db2641bad6274d750748c9b5b71ca82c5a7b0b62
PeterPiatek <piotrpiatek.gm@gmail.com>
2020-02-21 00:15:50 +0000

    sportsstore init

---
+++ sportsstore/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
